603398c11a826d5ef283015883d57625329ef084
ilGhera <andrea@ilghera.com>
2024-05-10 11:41:51 +0200

    Compatibilità HPOS in main file

--- crm-in-cloud-for-wc.php
+++ crm-in-cloud-for-wc.php
@@ -35,6 +35,16 @@ function crmfwc_wc_not_installed() {
 
 
 /**
+ * HPOS
+ */
+add_action( 'before_woocommerce_init', function() {
+	if ( class_exists( \Automattic\WooCommerce\Utilities\FeaturesUtil::class ) ) {
+		\Automattic\WooCommerce\Utilities\FeaturesUtil::declare_compatibility( 'custom_order_tables', __FILE__, true );
+	}
+} );
+
+
+/**
  * Handles the plugin activation
  *
  * @return void
