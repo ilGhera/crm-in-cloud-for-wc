9e3bd6865ee9add005ad84727173173857c60562
Andrea <andrea@ilghera.com>
2026-08-27 05:30:26 +0200

    chore: allinea Tested up to 7.1 nel main file al readme [skip-changelog]

--- crm-in-cloud-for-wc.php
+++ crm-in-cloud-for-wc.php
@@ -6,7 +6,7 @@
  * Version: 1.2.3
  * Stable tag: 1.2.3
  * Requires at least: 5.0
- * Tested up to: 6.8
+ * Tested up to: 7.1
  * WC tested up to: 11.0.1
  * Author: ilGhera
  * Author URI: https://ilghera.com
